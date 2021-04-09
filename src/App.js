471edf8de3fd8cff0b891ae2608e1964985e1d70
eliauk <wangkai02@sinovatech.com>
2021-04-09 10:57:39 +0800

    更改

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Tree, Input } from 'antd';
 
-function App() {
+const treeData = [
+  {
+    title: '0-0',
+    key: '0-0',
+    children: [
+      {
+        title: '0-0-0',
+        key: '0-0-0',
+        children: [
+          { title: '0-0-0-0', key: '0-0-0-0' },
+          { title: '0-0-0-1', key: '0-0-0-1' },
+          { title: '0-0-0-2', key: '0-0-0-2' },
+        ],
+      },
+      {
+        title: '0-0-1',
+        key: '0-0-1',
+        children: [
+          { title: '0-0-1-0', key: '0-0-1-0' },
+          { title: '0-0-1-1', key: '0-0-1-1' },
+          { title: '0-0-1-2', key: '0-0-1-2' },
+        ],
+      },
+      {
+        title: '0-0-2',
+        key: '0-0-2',
+      },
+    ],
+  },
+  {
+    title: '0-1',
+    key: '0-1',
+    children: [
+      { title: '0-1-0-0', key: '0-1-0-0' },
+      { title: '0-1-0-1', key: '0-1-0-1' },
+      { title: '0-1-0-2', key: '0-1-0-2' },
+    ],
+  },
+  {
+    title: '0-2',
+    key: '0-2',
+  },
+]
+const Demo = () => {
+  const [value, setValue] = useState('');
+  const [flag, setFlag] = useState([]);
+
+  const renderTree = (data = []) => {
+    return data.map((item) => {
+      const title = (
+        <span>
+          {flag[item.key] ? (
+            <Input
+              value={value}
+              onBlur={() => {
+                setFlag([])
+                item.title = value
+              }}
+              onChange={(e) => setValue(e.target.value)}
+            />
+          ) : (
+            <span
+              onClick={async () => {
+                let arr = [];
+                arr[item.key] = true;
+                setFlag(arr);
+                setValue(item.title);
+              }}
+            >
+              {item.title}
+            </span>
+          )}
+        </span>
+      );
+      if (item.children) {
+        return (
+          <Tree.TreeNode title={title} key={item.key}>
+            {renderTree(item.children)}
+          </Tree.TreeNode>
+        );
+      }
+      return <Tree.TreeNode title={title} key={item.key} />;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Tree>{renderTree(treeData)}</Tree>
   );
-}
-
-export default App;
+};
+export default Demo;
